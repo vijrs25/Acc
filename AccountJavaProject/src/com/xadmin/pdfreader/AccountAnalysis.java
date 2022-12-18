6feaa89ad4688e66c4a6b953c9755757136ccf01
vijay <linuxusir@gmail.com>
2022-12-18 18:14:36 +0530

    Printing on console and in text files update.

--- AccountJavaProject/src/com/xadmin/pdfreader/AccountAnalysis.java
+++ AccountJavaProject/src/com/xadmin/pdfreader/AccountAnalysis.java
@@ -2,6 +2,7 @@ package com.xadmin.pdfreader;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -11,6 +12,7 @@ import com.xadmin.tools.AccountTools;
 public class AccountAnalysis {
     
 	public static TreeMap<String, String[]> mixedMonthDatabase= new TreeMap<String, String[]>();
+	public static HashMap<String, String[]> individualUniquepeople= new HashMap<String, String[]>();
 	public static double lastCurrentAmount = 0;
 	static TreeMap<String, TreeMap<String, TransactionParameters>> listOfTransactions = new TreeMap<String, TreeMap<String, TransactionParameters>>();
 
@@ -19,15 +21,12 @@ public class AccountAnalysis {
 		try {
 
 			List<String> monthlyStatementString = new AccountTools().filesFromFolder();
-			// System.out.println(monthlyStatementString);
 
 			separatingAccount(monthlyStatementString);
-			printingTranction();
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
 	}
 
 	public static void separatingAccount(List<String> monthlyStatementString) {
@@ -45,67 +44,53 @@ public class AccountAnalysis {
 		String[] headers;
 		
 		String[] date = new String[3];
+		String name="";
+		String [] newHeader = null ;
 		for (String IndividualLine : tablesToLines) {
 			
 			    headers = IndividualLine.split(" ");		
 				String refrenceNum = "";
-				
 				for (int i = 0; i < headers .length; i++) {
 
 					if (headers[i].matches("../../..")) {
 						date = headers[i].split("/");
 					}
+					
+					if(headers[i].matches("../../..") && headers.length ==2) {
+						name = headers[1];
+						System.out.println(name);
+						newHeader = changeHeader(headers,name);
+						
+					}
 					if ((headers[i].matches("^0000\\d{12}"))) {
-
+//						if (date.length == 0) {
+//							yearMonthRefrence = date[2] + "-" + date[1] + "-" + date[0] + "_" + refrenceNum;
+//							mixedMonthDatabase.put(yearMonthRefrence, newHeader);
+//
+//						}
 						refrenceNum = headers[i];
-						headers[1]+=headers[2];
+						headers[1] += headers[2];
 						yearMonthRefrence = date[2] + "-" + date[1] + "-" + date[0] + "_" + refrenceNum;
 						mixedMonthDatabase.put(yearMonthRefrence, headers);
-
+						individualUniquepeople.put(headers[1], headers);
 						break;
 					}
 				}
 			}
 		printOnConsole(mixedMonthDatabase);
+		printOnConsole(individualUniquepeople);
+		printingTranction(mixedMonthDatabase);
+		printingTranction(individualUniquepeople);
 
 	}
 	
-	public static void forLongName() {
-		  
-	}
-
-	public static void printOnConsole(TreeMap<String, String[]> _monthlyDatabase ) {
-		int count=0;
-		System.out.println("sdf");
-		for (Entry<String, String[]> entry : _monthlyDatabase.entrySet()) {
-			System.out.println(++count+" "+entry.getKey()+" "+entry.getValue()[1]);
-		}
-	}
-	
-	
-
-	public static void printingTranction() {
-
+	private static void printingTranction(HashMap<String, String[]> _individualUniquepeople) {
 		try {
 
-			FileWriter myWriter = new FileWriter("C:\\Users\\vijay\\OneDrive\\Desktop\\filename2.txt");
-			myWriter.write("Files in Java adsfasdfasf\n\n");
+			FileWriter myWriter = new FileWriter("C:\\Programming\\java\\InidividualTransaction.txt");
 
-			// System.out.println(listOfTransactions);
-
-			for (Map.Entry<String, TreeMap<String, TransactionParameters>> month : listOfTransactions.entrySet()) {
-				// System.out.println(month.getKey()+" "+month.getValue());
-
-				for (Map.Entry<String, TransactionParameters> daily : month.getValue().entrySet()) {
-//						System.out.print(daily.getKey()+" "+daily.getValue().getRefrence()+"    debit =  "+daily.getValue().getDebited()+"  credit = "+daily.getValue().getCredited());
-//					
-					System.out.printf("%s %s %20s %20s", daily.getKey(), daily.getValue().getRefrence(),
-							daily.getValue().getDebited(), daily.getValue().getCredited());
-					System.out.println();
-					myWriter.write(daily.getKey() + " ------ " + daily.getValue().getRefrence() + " ======== "
-							+ daily.getValue().getDebited() + " -------- " + daily.getValue().getCredited() + "\n");
-
-				}
+			for (Entry<String, String[]> entry : _individualUniquepeople.entrySet()) {
+				myWriter.write(entry.getKey() + " " + entry.getValue()[1] + " " + "\n");
 			}
 
 			myWriter.close(); // closing try block
@@ -113,8 +98,48 @@ public class AccountAnalysis {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		// closing try block
+	}
 
+
+	private static String[] changeHeader(String[] _headers, String name) {
+		     String arr[] = new String[_headers.length+1];
+		     arr = _headers;
+		     arr[arr.length-1] = name;
+		return arr;
+	}
+
+	public static void printOnConsole(TreeMap<String, String[]> _monthlyDatabase ) {
+		int count=0;
+		System.out.println("printOnConsole");
+		for (Entry<String, String[]> entry : _monthlyDatabase.entrySet()) {
+			System.out.println(++count+" "+entry.getKey()+" "+entry.getValue()[1]);
+		}
+	}
+	
+	public static void printOnConsole(HashMap<String, String[]> _monthlyDatabase ) {
+		int count=0;
+		System.out.println("printOnConsole");
+		for (Entry<String, String[]> entry : _monthlyDatabase.entrySet()) {
+			System.out.println(++count+" "+entry.getKey()+" "+entry.getValue()[1]);
+		}
+	}
+	
+
+	public static void printingTranction(TreeMap<String, String[]> _mixedMonthDatabase) {
+
+		try {
+
+			FileWriter myWriter = new FileWriter("C:\\Programming\\java\\accountStatement.txt");
+
+			for (Entry<String, String[]> entry : _mixedMonthDatabase.entrySet()) {
+				myWriter.write(entry.getKey() + " " + entry.getValue()[1] + " " + "\n");
+			}
+
+			myWriter.close(); // closing try block
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
